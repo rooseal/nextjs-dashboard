3c0fdca23896d883fe6c993d8442762176a1ab16
Alyssa Roose <rooseal@aarixa.be>
2024-05-06 09:40:48 +0200

    Add error handling

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,20 +1,26 @@
+import { notFound } from "next/navigation";
+
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import EditInvoiceForm from "@/app/ui/invoices/edit-form";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 
-export default async function Page({ params }: { params: { id: string }}) {
+export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
 
+  if (!invoice) {
+    notFound();
+  }
+
   return (
     <main>
       <Breadcrumbs
         breadcrumbs={[
-          {label: 'Invoices', href: '/dashboard/invoices'},
-          {label: 'Edit Invoice', href: `/dashboard/invoices/${id}/edit`, active: true},
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          { label: 'Edit Invoice', href: `/dashboard/invoices/${id}/edit`, active: true },
         ]}
       />
       <EditInvoiceForm invoice={invoice} customers={customers} />
